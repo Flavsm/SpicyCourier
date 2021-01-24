a508ae128630ce1a216f893bd87e5e48c8923d76
Flavia Medeiros <flaardito@gmail.com>
2021-01-24 00:55:37 -0300

    Added JS, changes to css and html

---
+++ app.js
@@ -0,0 +1,41 @@
+//SELECTORS
+
+//EVENT LISTENERS
+
+//FUNCTIONS
+
+/* TO SELECT A MESSAGE
+there are 3 different databases, one for each type of message.
+after user chooses the type of message, it goes into a database.
+make a function with a simple if or switch statement should work.
+every message should correspond to an integer.
+by using math.random method, a random message should be picked.
+
+TO SEND AN EMAIL
+once user puts info, app should login to gmail(if it isnt already) 
+then create a new email
+then put recipients email on "send to"
+an automatic pre-made subject will be used for all messages
+then recipients name will be used to start the message
+the actual message comes next
+then finish email with users name
+and send
+
+
+have to create email account
+
+testing/examples:
+
+maybe a simple word doc on google docs or array
+
+anything goes database {
+
+}
+
+something nice databse {
+
+}
+
+make it weird database {
+
+} */
