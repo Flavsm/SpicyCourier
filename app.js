b9244798cd8d12d2ca3ece0c3e4a3da1ca7a4b52
Flavia Medeiros <flaardito@gmail.com>
2021-01-24 01:14:38 -0300

    fixed bug

--- app.js
+++ app.js
@@ -51,9 +51,9 @@ function sendEmail() {
     Email.send({
         SecureToken : "beb0a1d9-4cb4-4463-8669-f79377287a2a",
         To : `${recEmail}`,
-        From : "Spicy Courier",
+        From : "spicycourier@gmail.com",
         Subject : `Spicy Courier here - we got a message for you ${recName}`,
-        Body : `Hello ${recName}, what do you think? Hahahahaha! Cheers, ${sendName}`
+        Body : `Hello ${recName}, what do you think? Hahahahaha! Love you, ${sendName}`
     }).then(function(message){
         alert("The Courier is on its way!")
         location.reload(inputLabel);
